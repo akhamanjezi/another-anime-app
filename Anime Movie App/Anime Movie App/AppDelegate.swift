d2158dbb544f135d0f22efca2d0ac7ebe1209892
glucode-akha <147036193+glucode-akha@users.noreply.github.com>
2023-10-09 13:21:42 +0200

    Initial Project Setup (#2)
    * xcode project creation
    
    * ignore .DS_Store
    
    * replace scenedelete and main storyboard
    
    * various app configuration
    
    - App name
    - Device Orientation
    - Supported Devices
    
    * App icon
    
    * custom navigation controller with large title
    
    * basic launch screen

---
+++ Anime Movie App/Anime Movie App/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Anime Movie App
+//
+//  Created by Akha Manjezi on 9/10/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        setupWindow()
+        return true
+    }
+    
+    func setupWindow() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let navigationController = MainNavigationController(rootViewController: HomeViewController())
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+
+}
+
---
+++ Anime Movie App/Anime Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anime Movie App
+//
+//  Created by Akha Manjezi on 9/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
