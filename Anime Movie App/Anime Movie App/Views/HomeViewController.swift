0e89cdd6a7cdc96ff69bf65c8eaac89cdbd2dcc6
glucode-akha <akha@glucode.com>
2023-10-09 11:14:33 +0200

    replace scenedelete and main storyboard

---
+++ Anime Movie App/Anime Movie App/Views/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  Anime Movie App
+//
+//  Created by Akha Manjezi on 9/10/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    func setupView() {
+        self.title = "Animovie"
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
