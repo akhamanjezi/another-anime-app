eec57e224bdbd1769e50d47facb73d7005b83efb
glucode-akha <akha@glucode.com>
2023-10-11 12:19:17 +0200

    add searchingError state to homeviewmodel

--- Anime Movie App/Anime Movie App/DomainLayer/ViewModels/HomeViewModel.swift
+++ Anime Movie App/Anime Movie App/DomainLayer/ViewModels/HomeViewModel.swift
@@ -4,6 +4,7 @@ class HomeViewModel {
     private let animeRepository: AnimeRepository
     var animeSearchResults: Observable<[Anime]> = Observable([])
     var isSearching: Observable<Bool> = Observable(false)
+    var searchingError: Observable<LocalizedError?> = Observable(nil)
     
     init(animeRepository: AnimeRepository) {
         self.animeRepository = animeRepository
@@ -11,12 +12,14 @@ class HomeViewModel {
     
     func search(for searchTerm: String) {
         isSearching.value = true
+        searchingError.value = nil
+        
         animeRepository.getSearchResults(for: searchTerm) { result in
             switch result {
             case .success(let anime):
                 self.updateSearchResults(with: anime)
-            case .failure(_):
-                self.updateSearchResults(with: [])
+            case .failure(let error):
+                self.handleSearchingError(error)
             }
         }
     }
@@ -24,5 +27,10 @@ class HomeViewModel {
     private func updateSearchResults(with anime: [Anime]) {
         self.animeSearchResults.value = anime
         self.isSearching.value = false
+    }
+    
+    private func handleSearchingError(_ error: LocalizedError? = nil) {
+        self.updateSearchResults(with: [])
+        self.searchingError.value = error
     }
 }
