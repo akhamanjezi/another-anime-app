ee3d5249aa47b4af64e7e22d9ad2a966f88fcecb
glucode-akha <akha@glucode.com>
2023-10-11 11:27:55 +0200

    mock dataprovider and test KitsuRepository

---
+++ Anime Movie App/Anime Movie AppTests/KitsuRepositoryTests.swift
@@ -0,0 +1,63 @@
+import XCTest
+
+final class KitsuRepositoryTests: XCTestCase {
+    private func getRepositoryWith(file named: String) -> AnimeRepository {
+        let filePath = Bundle(for: Anime_Movie_AppTests.self).url(forResource: named, withExtension: "json")?.path(percentEncoded: false) ?? ""
+
+        let mockDP = MockDataProvider(resourcePath: filePath)
+        return KitsuRespository(dataProvider: mockDP)
+    }
+    
+    func testSuccessfullCallWithNotNullData() throws {
+        let kitsuRepository = getRepositoryWith(file: "spirited_away")
+        
+        kitsuRepository.getSearchResults(for: "") { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure(_):
+                XCTFail("Unexpected unsuccessful call")
+            }
+        }
+    }
+    
+    func testSuccessfullCallWithNullData() throws {
+        let kitsuRepository = getRepositoryWith(file: "null")
+                
+        kitsuRepository.getSearchResults(for: "") { result in
+            switch result {
+            case .success(_):
+                XCTFail("Unexpected successful call")
+            case .failure(let error):
+                XCTAssert(error == .invalidResponse)
+            }
+        }
+    }
+    
+    func testSuccessfulAnimeReturn() throws {
+        let kitsuRepository = getRepositoryWith(file: "spirited_away")
+        
+        kitsuRepository.getSearchResults(for: "") { result in
+            switch result {
+            case .success(let data):
+                let testData = Anime.testData()[0]
+                XCTAssertEqual(data[0], testData)
+            case .failure(_):
+                XCTFail("Unexpected unsuccessful call")
+            }
+        }
+    }
+    
+    func testUnSuccessfullCall() throws {
+        let kitsuRepository = getRepositoryWith(file: "")
+                
+        kitsuRepository.getSearchResults(for: "") { result in
+            switch result {
+            case .success(_):
+                XCTFail("Unexpected successful call")
+            case .failure(let error):
+                XCTAssert(error == .invalidRequest)
+            }
+        }
+    }
+}
