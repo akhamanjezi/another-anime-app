837df77823462608d87053866fe39335b70a597d
glucode-akha <akha@glucode.com>
2023-10-11 11:09:28 +0200

    remove vars from dataprovider protocol

--- Anime Movie App/Anime Movie App/DataLayer/Networking/DataProvider.swift
+++ Anime Movie App/Anime Movie App/DataLayer/Networking/DataProvider.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 protocol DataProvider {
-    
-    var baseURL: String {
-        get
-    }
-    
-    var searchEndpoint: String {
-        get
-    }
-    
     func search(for term: String, completion: @escaping (Result<Data, LocalizedError>) -> ())
 }
