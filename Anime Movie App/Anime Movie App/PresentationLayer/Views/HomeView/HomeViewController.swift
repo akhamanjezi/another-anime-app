e4ae2607604c165452cc5f2d9881d6023f3e2797
glucode-akha <akha@glucode.com>
2023-10-13 14:46:28 +0200

    home view layout with stored assets

---
+++ Anime Movie App/Anime Movie App/PresentationLayer/Views/HomeView/HomeViewController.swift
@@ -0,0 +1,86 @@
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDelegate {
+    private let viewModel = HomeViewModel()
+    @IBOutlet private weak var favouritesTableView: UITableView!
+    @IBOutlet private weak var featureImageView: UIImageView!
+    @IBOutlet private weak var favouritesLabel: UILabel!
+    @IBOutlet private weak var featureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupSearchResultsController()
+        registerNib()
+        updateFeatureAnime()
+    }
+    
+    private func setupView() {
+        self.title = "Animovie"
+        featureImageView.layer.cornerRadius = 8
+        favouritesLabel.font = .sectionTitle
+        featureLabel.font = .featureTitle
+    }
+    
+    private func setupSearchResultsController() {
+        let rvc = SearchTableViewController()
+        let search = UISearchController(searchResultsController: rvc)
+        search.delegate = rvc
+        search.searchBar.delegate = rvc
+        self.navigationItem.searchController = search
+    }
+    
+    private func registerNib() {
+        let nib = UINib(nibName: "SearchTableViewCell", bundle: nil)
+        favouritesTableView.register(nib, forCellReuseIdentifier: SearchTableViewCell.cellIdentifier)
+        
+        favouritesTableView.delegate = self
+        favouritesTableView.dataSource = self
+    }
+    
+    private func updateFeatureAnime() {
+        viewModel.newFeatureAnime { [weak self] anime in
+            self?.featureLabel.text = "\(anime.title ?? "") - \(anime.styledReleaseDate ?? "")"
+        }
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.cellIdentifier, for: indexPath) as! SearchTableViewCell
+        cell.configureCell(for: Anime(title: "Spirited Away",
+                                      genres: nil,
+                                      releaseDate: "2001-07-20".toDate(),
+                                      synopsis: "Stubborn, spoiled, and naïve, 10-year-old Chihiro Ogino is less than pleased when she and her parents discover an abandoned amusement park on the way to their new house. Cautiously venturing inside, she realizes that there is more to this place than meets the eye, as strange things begin to happen once dusk falls. Ghostly apparitions and food that turns her parents into pigs are just the start—Chihiro has unwittingly crossed over into the spirit world. Now trapped, she must summon the courage to live and work amongst spirits, with the help of the enigmatic Haku and the cast of unique characters she meets along the way.\nVivid and intriguing, Sen to Chihiro no Kamikakushi tells the story of Chihiro's journey through an unfamiliar world as she strives to save her parents and return home.\n[Written by MAL Rewrite]",
+                                      averageRating: 82.59,
+                                      ageRating: "G",
+                                      imageURL: "https://media.kitsu.io/anime/poster_images/176/original.jpg",
+                                      thumnail: nil,
+                                      duration: .seconds(60) * 125,
+                                      externalID: "176",
+                                      source: .kitsu))
+        return cell
+    }
+    
+    private func configureCell(_ cell: SearchTableViewCell, for anime: Anime) -> SearchTableViewCell {
+        cell.titleLabel.text = anime.title
+        cell.thumbnailView.image = UIImage(named: "posterImage")
+        cell.thumbnailView.contentMode = .scaleAspectFill
+        
+        return cell
+    }
+}
+
+fileprivate extension SearchTableViewCell {
+    func configureCell(for anime: Anime) {
+        self.titleLabel.text = anime.title
+        self.releaseDateLabel.text = anime.styledReleaseDate
+        self.thumbnailView.image = UIImage(named: "posterImage")
+        self.thumbnailView.contentMode = .scaleAspectFill
+    }
+}
