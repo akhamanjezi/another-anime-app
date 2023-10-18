9e28328258c112a30a1decb83b1c87b5c27a3fab
glucode-akha <akha@glucode.com>
2023-10-18 23:00:36 +0200

    View controller and model for showing an anime’s details
    todo: more details

---
+++ Anime Movie App/Anime Movie App/PresentationLayer/ViewModels/DetailsViewModel.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class DetailsViewModel {
+    let anime: Anime
+    
+    init(anime: Anime) {
+        self.anime = anime
+    }
+}
---
+++ Anime Movie App/Anime Movie App/PresentationLayer/Views/DetailsView/DetailsViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+class DetailsViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet private weak var detailsTableView: UITableView!
+    private let viewModel: DetailsViewModel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        registerNibs()
+        configureTableView()
+    }
+    
+    init(with viewModel: DetailsViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: "DetailsViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.navigationItem.title = viewModel.anime.title
+        self.navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    private func registerNibs() {
+        let titleDescriptionTableViewCellNib = UINib(nibName: "TitleDescriptionTableViewCell", bundle: nil)
+        detailsTableView.register(titleDescriptionTableViewCellNib, forCellReuseIdentifier: TitleDescriptionTableViewCell.cellIdentifier)
+        let imageDescriptionTableViewCell = UINib(nibName: "ImageDescriptionTableViewCell", bundle: nil)
+        detailsTableView.register(imageDescriptionTableViewCell, forCellReuseIdentifier: ImageDescriptionTableViewCell.cellIdentifier)
+    }
+    
+    private func configureTableView() {
+        detailsTableView.delegate = self
+        detailsTableView.dataSource = self
+    }
+}
+
+// MARK: UITableViewDataSource
+
+extension DetailsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            return imageDescriptionCell(for: tableView, cellForRowAt: indexPath)
+        }
+        
+        return titleDescriptionCell(for: tableView, cellForRowAt: indexPath, title: "Synopsis", description: viewModel.anime.synopsis)
+    }
+    
+    private func imageDescriptionCell(for tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ImageDescriptionTableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImageDescriptionTableViewCell.cellIdentifier, for: indexPath) as! ImageDescriptionTableViewCell
+        cell.configureCell(for: viewModel.anime)
+        return cell
+    }
+    
+    private func titleDescriptionCell(for tableView: UITableView, cellForRowAt indexPath: IndexPath, title: String, description: String?) -> TitleDescriptionTableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TitleDescriptionTableViewCell.cellIdentifier, for: indexPath) as! TitleDescriptionTableViewCell
+        cell.configureCell(title: title, description: description)
+        return cell
+    }
+}
