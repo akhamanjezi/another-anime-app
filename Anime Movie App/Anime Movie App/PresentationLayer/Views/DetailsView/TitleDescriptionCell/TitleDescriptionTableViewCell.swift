8f74ec4497f4218793c13e68b8f6026417859ab5
glucode-akha <akha@glucode.com>
2023-10-18 22:56:25 +0200

    Title and description view for details page

---
+++ Anime Movie App/Anime Movie App/PresentationLayer/Views/DetailsView/TitleDescriptionCell/TitleDescriptionTableViewCell.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class TitleDescriptionTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    static let cellIdentifier = "titleDescriptionTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func configureCell(title: String?, description: String?) {
+        self.titleLabel.text = title
+        self.descriptionLabel.text = description
+    }
+    
+    private func setupView() {
+        titleLabel.font = .roundedCallout
+        descriptionLabel.font = .roundedCaption2
+    }
+}
