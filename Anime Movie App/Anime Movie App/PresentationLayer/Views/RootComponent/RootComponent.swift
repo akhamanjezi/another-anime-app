2248625bd66f77700c207c1008d9a886a475b19d
glucode-akha <akha@glucode.com>
2024-02-19 10:07:07 +0200

    Init dependencies instead of defaults

--- Anime Movie App/Anime Movie App/PresentationLayer/Views/RootComponent/RootComponent.swift
+++ Anime Movie App/Anime Movie App/PresentationLayer/Views/RootComponent/RootComponent.swift
@@ -11,7 +11,51 @@ class RootComponent: BootstrapComponent {
     }
     
     public var homeViewModel: HomeViewModel {
-        HomeViewModel()
+        HomeViewModel(animeRepository: animeRepository)
+    }
+    
+    var animeRepository: AnimeRepository {
+        KitsuRepository(dataProvider: dataProvider,
+                        toAnimeMapper: toAnimeMapper,
+                        imageRepository: imageRepository,
+                        favouritesManager: favouritesManager)
+    }
+    
+    private var dataProvider: DataProviding {
+        KitsuProvider()
+    }
+    
+    private var toAnimeMapper: any ToAnimeMapper<KitsuResult> {
+        KitsuResultToAnimeMapper()
+    }
+    
+    private var imageRepository: any ImageRepository {
+        ImageRepo(imageDownloader: imageDownloader,
+                  storage: imageStorage)
+    }
+    
+    private var imageDownloader: ImageDownloading {
+        ImageDownloader()
+    }
+    
+    private var imageStorage: any ImageStoring<NSURL, UIImage> {
+        shared {
+            ImageCache(cache: NSCache<NSURL, UIImage>())
+        }
+    }
+    
+    private var favouritesManager: any FavouritesManaging {
+        FavouritesManager(storage: favouritesStorage,
+                          mapper: savedAnimeToAnimeMapper)
+    }
+    
+    private var favouritesStorage: any DataStoring<String, Data> {
+        shared {
+            FavouritesStorage(storage: UserDefaults.standard)
+        }
+    }
+    
+    private var savedAnimeToAnimeMapper: any BidirectionalAnimeMapping<SavedAnime> {
+        SavedAnimeToAnimeMapper()
     }
 }
-
