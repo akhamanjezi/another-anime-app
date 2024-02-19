f8083bc5bd6c7a801657aa5d06b8fe4b9ac187c5
glucode-akha <akha@glucode.com>
2024-02-19 09:17:50 +0200

    Needle generate for home + root component

---
+++ Anime Movie App/Anime Movie App/NeedleGenerated.swift
@@ -0,0 +1,72 @@
+
+
+import NeedleFoundation
+import UIKit
+
+// swiftlint:disable unused_declaration
+private let needleDependenciesHash : String? = nil
+
+// MARK: - Traversal Helpers
+
+private func parent1(_ component: NeedleFoundation.Scope) -> NeedleFoundation.Scope {
+    return component.parent
+}
+
+// MARK: - Providers
+
+#if !NEEDLE_DYNAMIC
+
+private class HomeDependencycad225e9266b3c9a56ddProvider: HomeDependency {
+    var homeViewModel: HomeViewModel {
+        return rootComponent.homeViewModel
+    }
+    private let rootComponent: RootComponent
+    init(rootComponent: RootComponent) {
+        self.rootComponent = rootComponent
+    }
+}
+/// ^->RootComponent->HomeComponent
+private func factory7cf6ef49229ffaf97a15b3a8f24c1d289f2c0f2e(_ component: NeedleFoundation.Scope) -> AnyObject {
+    return HomeDependencycad225e9266b3c9a56ddProvider(rootComponent: parent1(component) as! RootComponent)
+}
+
+#else
+extension RootComponent: Registration {
+    public func registerItems() {
+
+        localTable["homeViewModel-HomeViewModel"] = { [unowned self] in self.homeViewModel as Any }
+    }
+}
+extension DetailsComponent: Registration {
+    public func registerItems() {
+
+    }
+}
+extension HomeComponent: Registration {
+    public func registerItems() {
+        keyPathToName[\HomeDependency.homeViewModel] = "homeViewModel-HomeViewModel"
+
+    }
+}
+
+
+#endif
+
+// MARK: - Registration
+private func registerProviderFactory(_ componentPath: String, _ factory: @escaping (NeedleFoundation.Scope) -> AnyObject) {
+    __DependencyProviderRegistry.instance.registerDependencyProviderFactory(for: componentPath, factory)
+}
+
+#if !NEEDLE_DYNAMIC
+
+@inline(never) private func register1() {
+    registerProviderFactory("^->RootComponent", factoryEmptyDependencyProvider)
+    registerProviderFactory("^->RootComponent->HomeComponent", factory7cf6ef49229ffaf97a15b3a8f24c1d289f2c0f2e)
+}
+#endif
+
+public func registerProviderFactories() {
+#if !NEEDLE_DYNAMIC
+    register1()
+#endif
+}
